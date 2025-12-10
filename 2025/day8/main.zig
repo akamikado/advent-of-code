c37dc9b2794299a99ebbea276e252eb1cd1f4b42
akamikado <manishvasireddy1234@gmail.com>
2025-12-11 03:24:11 +0530

    cleaned day 8 code

--- 2025/day8/main.zig
+++ 2025/day8/main.zig
@@ -4,6 +4,42 @@ fn distance(a: [3]f32, b: [3]f32) f32 {
     return @sqrt((a[0] - b[0])*(a[0] - b[0]) + (a[1] - b[1])*(a[1] - b[1]) + (a[2] - b[2])*(a[2] - b[2]));
 }
 
+const Context = struct {
+    dist: ?f32,
+    junctions: ?*std.array_list.Aligned([3]f32, null),
+    size: ?*[]usize
+};
+
+fn sortFn(ctx: Context, a: usize, b: usize) bool {
+    return (ctx.size.?.*)[a] > (ctx.size.?.*)[b];
+}
+
+fn boundFn(ctx: Context, idx: [2]usize) std.math.Order {
+    const curDist = distance(ctx.junctions.?.items[idx[0]], ctx.junctions.?.items[idx[1]]);
+    return std.math.order(ctx.dist.?, curDist);
+}
+
+const disjointSet = struct {
+    pub fn join(parents: []usize, size: []usize, u: usize, v: usize) void {
+        const pu = findParent(parents, u);
+        const pv = findParent(parents, v);
+
+        if (pu == pv) return;
+        if (size[pu] < size[pv]) {
+            parents[pu] = pv;
+            size[pv] += size[pu];
+        } else {
+            parents[pv] = pu;
+            size[pu] += size[pv];
+        }
+    }
+    pub fn findParent(parents: []usize, node: usize) usize {
+        if (node == parents[node]) return node;
+        parents[node] = findParent(parents, parents[node]);
+        return parents[node];
+    }
+};
+
 pub fn main() !void {
     var fileBuf: [4096]u8 = undefined;
     var file = try std.fs.cwd().openFile("day8/input.txt", .{});
@@ -45,32 +81,12 @@ pub fn main() !void {
         }
     }
 
-    const disjointSet = struct {
-        pub fn join(parents: []usize, size: []usize, u: usize, v: usize) void {
-            const pu = findParent(parents, u);
-            const pv = findParent(parents, v);
-
-            if (pu == pv) return;
-            if (size[pu] < size[pv]) {
-                parents[pu] = pv;
-                size[pv] += size[pu];
-            } else {
-                parents[pv] = pu;
-                size[pu] += size[pv];
-            }
-        }
-        pub fn findParent(parents: []usize, node: usize) usize {
-            if (node == parents[node]) return node;
-            parents[node] = findParent(parents, parents[node]);
-            return parents[node];
-        }
-    };
-
     var parents = try allocator.alloc(usize, junctions.items.len);
     defer allocator.free(parents);
     for (0..junctions.items.len) |i| {
         parents[i] = i;
     }
+
     var size = try allocator.alloc(usize, junctions.items.len);
     defer allocator.free(size);
     @memset(size, 1);
@@ -79,22 +95,6 @@ pub fn main() !void {
     
     while (true) {
         var sortedPairs: [maxConns][2]usize = [1][2]usize{maxDistPair} ** maxConns;
-
-        const Context = struct {
-            dist: ?f32,
-            junctions: ?*std.array_list.Aligned([3]f32, null),
-            size: ?*[]usize
-        };
-        const cmp = struct {
-            pub fn bound(ctx: Context, idx: [2]usize) std.math.Order {
-                const curDist = distance(ctx.junctions.?.items[idx[0]], ctx.junctions.?.items[idx[1]]);
-                return std.math.order(ctx.dist.?, curDist);
-            }
-
-            pub fn sort(ctx: Context, a: usize, b: usize) bool {
-                return (ctx.size.?.*)[a] > (ctx.size.?.*)[b];
-            }
-        };
 
         for (0..junctions.items.len) |i| {
             for (i+1..junctions.items.len) |j| {
@@ -105,9 +105,9 @@ pub fn main() !void {
                     .junctions = &junctions,
                     .size = null
                 };
-                const idx = std.sort.upperBound([2]usize, &sortedPairs, ctx, cmp.bound);
+                const idx = std.sort.upperBound([2]usize, &sortedPairs, ctx, boundFn);
                 if (idx >= maxConns) continue;
-                @memmove(sortedPairs[idx+1..], sortedPairs[idx..maxConns-1]);
+                @memmove(sortedPairs[idx+1..maxConns], sortedPairs[idx..maxConns-1]);
                 sortedPairs[idx] = [_]usize{i, j};
             }
         }
@@ -125,28 +125,25 @@ pub fn main() !void {
             }
             if (numCircuits == 1) {
                 lastProd = @as(u64, @intFromFloat(junctions.items[sortedPairs[i][0]][0])) * @as(u64, @intFromFloat(junctions.items[sortedPairs[i][1]][0]));
-                break;
+                std.debug.print("Part 2: {d}\n", .{lastProd});
+                return;
             }
         }
 
         var circuits = try std.ArrayListUnmanaged(usize).initCapacity(allocator, junctions.items.len);
         defer circuits.deinit(allocator);
+
         for (0..junctions.items.len) |i| {
             if (i == disjointSet.findParent(parents, i)) try circuits.append(allocator, i);
         }
-        if (circuits.items.len == 1) {
-            std.debug.print("Part 2: {d}\n", .{lastProd});
-            break;
-        }
-
-        const ctx: Context = .{ 
-            .dist = null,
-            .junctions = null,
-            .size = &size
-        };
-        std.mem.sort(usize, circuits.items, ctx, cmp.sort);
 
         if (minBound == 0) {
+            const ctx: Context = .{ 
+                .dist = null,
+                .junctions = null,
+                .size = &size
+            };
+            std.mem.sort(usize, circuits.items, ctx, sortFn);
             var ans: u64 = 1;
             for (0..@min(circuits.items.len, 3)) |i| {
                 ans *= size[circuits.items[i]];
